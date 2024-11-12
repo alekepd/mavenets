4b4e91d099fba9d270763acd4b0a173a821c1df3
Aleksander Durumeric <alekepd@gmail.com>
2024-11-12 07:44:51 -0600

    Added MAX_DATASPEC_INDEX to export list of data submodule.

--- src/mavenets/data/__init__.py
+++ src/mavenets/data/__init__.py
@@ -1,4 +1,4 @@
 """Provides interface to training, validation, and test data."""
-from .load import get_datasets # noqa: F401
-from .spec import resolve_dataspec, DataSpec # noqa: F401
-from .graph import LegacyGraphDataReader #noqa: F401
+from .load import get_datasets  # noqa: F401
+from .spec import resolve_dataspec, DataSpec, MAX_DATASPEC_INDEX  # noqa: F401
+from .graph import LegacyGraphDataReader  # noqa: F401
