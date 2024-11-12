56347b31987c8bad1dd89411be2265d18f8ff5b1
Aleksander Durumeric <alekepd@gmail.com>
2024-11-12 14:31:21 -0600

    Documented tools module and marked functions as private.

--- src/mavenets/tools.py
+++ src/mavenets/tools.py
@@ -11,15 +11,12 @@ import pandas as pd  # type: ignore
 from .util import null_contextmanager, sequence_mean, Patience
 from .network import MHTuner
 
-# was this: loss = cal_loss + mix * raw_loss
-# for later: loss = mix_cal * cal_loss + mix_raw * raw_loss
-
 SIGNAL_PYGBATCHKEY: Final = "y"
 EXP_PYGBATCHKEY: Final = "experiment"
 
 
 class DoubleParameterizedLoss(Protocol):
-    """Specialized loss function.
+    """Protocol for specialized loss function.
 
     As input, takes a 2-tuple of tensors for the signal, a single tensor for the
     reference, and an additional tensor to control the output.
@@ -36,7 +33,7 @@ class DoubleParameterizedLoss(Protocol):
 
 
 class ParamTrainEval(Protocol):
-    """Callable that minimizes a model.
+    """Protocol for callable that minimizes a model.
 
     As input, takes model input, reference value, and a tensor passed to the
     loss function controlling its behavior.
@@ -57,11 +54,30 @@ def mixed_MSE(
     reference: torch.Tensor,
     mix: torch.Tensor,
 ) -> torch.Tensor:
+    """Calculate MSE loss on two inputs to a single reference and linearly combine them.
+
+    Allows for training with an "annealing" schedule that switches the loss balance.
+
+    Arguments:
+    ---------
+    guesses:
+        2-Tuple, each element is a prediction.
+    reference:
+        Reference both inputs are compared against.
+    mix:
+        MSE losses are combined via (1.0-mix) and mix.
+
+    Returns:
+    -------
+    Tensor with mean MSE.
+
+    """
     first_mse = torch.nn.functional.mse_loss(guesses[0], reference)
     second_mse = torch.nn.functional.mse_loss(guesses[1], reference)
     return (1.0 - mix) * first_mse + mix * second_mse
 
-def create_parameterized_train_stepper(
+
+def _create_parameterized_train_stepper(
     *,
     model: torch.nn.Module,
     optimizer: torch.optim.Optimizer,
@@ -72,7 +88,36 @@ def create_parameterized_train_stepper(
     compile_mode: Optional[str] = None,
     grad_clip: float = 1e3,
 ) -> ParamTrainEval:
+    """Create function that can be called to perform an optimization step.
 
+    Keyword only arguments.
+
+    Arguments:
+    ---------
+    model:
+        torch.nn.Module to optimize.
+    optimizer:
+        torch.optim optimizer (must already have been initialized)
+    loss_function:
+        Loss function that must take the entire model output as the first argument,
+        reference as the second argument, and a third argument modulating its behavior.
+    device:
+        string specifying which device to use in wrapped function. Must be the same
+        as the device of tensors layter supplied to returned function.
+    bfloat16:
+        If true, bf16 is used for part of the forward pass.
+    compile:
+        If true, compile the returned function.
+    compile_mode:
+        Passed to torch.compile if compile is True.
+    grad_clip:
+        Gradient norm to clip at.
+
+    Returns:
+    -------
+    Function that can be called to
+
+    """
     if bfloat16:
         precision_manager: Any = torch.autocast
     else:
@@ -120,7 +165,7 @@ class _MHTunerWrapper(torch.nn.Module):
             return self.tuned_model(inp[0], inp[1], return_raw=True)
 
 
-def create_evaler(
+def _create_parameterized_evaler(
     *,
     model: torch.nn.Module,
     loss_function: Callable[..., torch.Tensor],
@@ -128,9 +173,38 @@ def create_evaler(
     bfloat16: bool = False,
     compile: bool = False,
     compile_mode: Optional[str] = None,
-    graph: bool = False,
 ) -> ParamTrainEval:
+    """Create function that can be called to evaluate a nn.Module on to obtain a loss.
 
+    Keyword only arguments.
+
+    Similar to create_parameterized_train_stepper, but model is evaluated in eval mode
+    and no optimization is performed. Useful to back in autocasting and pass compilation
+    arguments.
+
+    Arguments:
+    ---------
+    model:
+        nn.Module to evaluate.
+    loss_function:
+        Loss function that must take the entire model output as the first argument,
+        reference as the second argument, and a third argument modulating its behavior.
+    device:
+        string specifying which device to use in wrapped function. Must be the same
+        as the device of tensors layter supplied to returned function.
+    bfloat16:
+        If true, bf16 is used for part of the forward pass.
+    compile:
+        If true, compile the returned function.
+    compile_mode:
+        If compile is True, passed to torch.compile.
+
+    Returns:
+    -------
+    Callable that can map input and reference tensors, along with a loss
+    parameter, to a loss value.
+
+    """
     if bfloat16:
         precision_manager: Any = torch.autocast
     else:
@@ -161,7 +235,7 @@ def _eval_dataset(
     loss_param: torch.Tensor,
     graph: bool = False,
 ) -> float:
-    """Loader and evaluate loss on given data."""
+    """Load and evaluate loss on given data."""
     if graph:
         loader = pygDataLoader(
             dataset,
@@ -213,7 +287,79 @@ def train_tunable_model(
     patience: int = 50,
     graph: bool = False,
 ) -> Tuple[int, float, pd.DataFrame]:
+    """Train MHTuner instance.
 
+    Arguments:
+    ---------
+    model:
+        Model that will be optimized. Must be a MHTuner instance, not just a nn.Module.
+    optimizer:
+        torch.optim optimizer instance. Must already be initialized.
+    device:
+        Device to train on; likely "cpu" or "cuda".
+    n_epochs:
+        Maximum number of epochs to train for. If no improvement in the validation loss
+        is observed over a given time frame (see patience argument), training
+        may be halted before reading this number of epochs.
+    train_dataset:
+        Validation dataset. If graph is True, should be serve pyg Data objects with
+        x, y, and experiment attributes. Else, should return
+        (features, signal, experiment) tuples.
+    valid_dataset:
+        Validation dataset. If graph is True, should be serve pyg Data objects with
+        x, y, and experiment attributes. Else, should return
+        (features, signal, experiment) tuples.
+    train_loss_function:
+        Train loss function to use. Must be a parameterized loss: should map
+        three arguments (guess, reference, parameter) to a scalar tensor.
+        This is likely an instance of mixed_MSE.
+    report_loss_function:
+        Train loss function to use. Must be a parameterized loss: should map
+        three arguments (guess, reference, parameter) to a scalar tensor.
+        This is likely an instance of mixed_MSE.
+    train_batch_size:
+        Batch size for training.
+    reporting_batch_size:
+        Batch size for reporting losses.
+    report_stride:
+        Loss reporting is done at intevals of this size. E.g., if 5, we report losses
+        every 5 epochs. Larger values result in faster training, but less information.
+    compile:
+        Whether to compile the train and eval calls.
+    compile_mode:
+        Passed to torch.compile if compile is True.
+    train_bfloat16:
+        Whether to run forward pass in bfloat16 precision. May not be supported on all
+        GPUs.
+    start_loss_param:
+        Optimization is performed using a loss change changes during training by passing
+        a control parameter. This parameter starts at start_loss_param and linearly
+        ramps down to end_loss_param over loss_param_ramp_size epochs; after this ramp,
+        the parameter stays at end_loss for the remainder of training.
+    end_loss_param:
+        See start_loss_param.
+    loss_param_ramp_size:
+        See start_loss_param.
+    grad_clip:
+        Grad norm to clip at. Lower this is training all of a sudden becomes unstable.
+    progress_bar:
+        Whether to show a tqdm progress bar updating every epoch.
+    patience:
+        Training is terminated one the (non-smoothed) validation loss has not improved
+        for this many updates (not epochs--- epochs*report_stride).
+    graph:
+        Treats model as a torch geometric graph neural network. Effectively, if
+        model take torch_geometric Data objects as input, this must be True. Changes
+        how models are wrapped and objects used for data loading.
+
+    Returns:
+    -------
+    3-Tuple: First element is the epoch where the optimal validation loss is found
+    after smoothing data with 3-item rolling median, second element is the smoothed
+    loss at that epoch, and third is a pd.DataFrame containing logged training and
+    validation losses.
+
+    """
     loss_param_schedule = list(
         torch.linspace(
             start_loss_param, end_loss_param, loss_param_ramp_size, device=device
@@ -222,7 +368,7 @@ def train_tunable_model(
 
     wrapped_model = _MHTunerWrapper(model, graph=graph)
 
-    train_stepper = create_parameterized_train_stepper(
+    train_stepper = _create_parameterized_train_stepper(
         model=wrapped_model,
         optimizer=optimizer,
         device=device,
@@ -233,7 +379,7 @@ def train_tunable_model(
         grad_clip=grad_clip,
     )
 
-    evaler = create_evaler(
+    evaler = _create_parameterized_evaler(
         model=wrapped_model,
         device=device,
         loss_function=report_loss_function,
