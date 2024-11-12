dc83111adac09ec8e5f69476ea5ab54652006742
Aleksander Durumeric <alekepd@gmail.com>
2024-11-12 07:58:13 -0600

    Refined integer encoding module with documention and sanity checks.

--- src/mavenets/data/featurize.py
+++ src/mavenets/data/featurize.py
@@ -1,6 +1,6 @@
-"""Routines to featurize data."""
+"""Routines to featurize sequence data."""
 
-from typing import List, Dict, overload, Literal, Union
+from typing import List, Dict, overload, Literal, Union, Iterable
 import pandas as pd  # type: ignore
 import torch
 
@@ -11,20 +11,66 @@ def get_alphabet(frame: pd.DataFrame, column_name: str) -> List[str]:
 
 
 def encoder_dict(alphabet: List[str]) -> Dict[str, int]:
-    """Create encoder dictionary."""
+    """Create encoder dictionary.
+
+    Arguments:
+    ---------
+    alphabet:
+        list of letters in dictionary. Assumed have unique elements.
+
+    Returns:
+    -------
+    Dictionary mapping alphabet members to integers.
+
+    """
     return {letter: ind for ind, letter in enumerate(alphabet)}
 
 
 def decoder_dict(alphabet: List[str]) -> Dict[int, str]:
-    """Create encoder dictionary."""
+    """Create encoder dictionary.
+
+    Arguments:
+    ---------
+    alphabet:
+        list of letters in dictionary. Assumed have unique elements.
+
+    Returns:
+    -------
+    Dictionary mapping integers to alphabet members.
+
+    """
     return dict(enumerate(alphabet))
+
+
+def _sanity_check() -> None:
+    letters = ["A", "D", "E"]
+    enc = encoder_dict(letters)
+    dec = decoder_dict(letters)
+    for let in letters:
+        encoded = enc[let]
+        if dec[encoded] != let:
+            raise ValueError(
+                "Encoder does not satisfy round-trip equivalence in sanity check."
+            )
+
+
+_sanity_check()
 
 
 class IntEncoder:
     """Encodes strings as integer lists or tensors."""
 
     def __init__(self, alphabet: List[str]) -> None:
-        """Create backing dictionaries."""
+        """Create backing dictionaries.
+
+        Arguments:
+        ---------
+        alphabet:
+            list of letters in dictionary. Must have unqiue elements.
+
+        """
+        if len(set(alphabet)) != len(alphabet):
+            raise ValueError("Alphabet does not comprise unique elements.")
         self.encode_map = encoder_dict(alphabet)
         self.decode_map = decoder_dict(alphabet)
 
@@ -37,15 +83,32 @@ class IntEncoder:
         ...
 
     def encode(self, target: str, tensor: bool) -> Union[List[int], torch.Tensor]:
-        """Encode single example."""
+        """Encode single example.
+
+        Arguments:
+        ---------
+        target:
+            Single string to encode.
+        tensor:
+            If true, encoded content is returned as a Tensor. Else, returned as a
+            list of ints.
+
+        Returns:
+        -------
+        List of integers or a Tensor.
+
+        """
         encoded = [self.encode_map[x] for x in target]
         if tensor:
             return torch.tensor(encoded, dtype=torch.int32)
         else:
             return encoded
 
-    def batch_encode(self, targets: List[str]) -> torch.Tensor:
-        """Encode multiple examples into single tensor."""
+    def batch_encode(self, targets: Iterable[str]) -> torch.Tensor:
+        """Encode multiple examples into single tensor.
+
+        Only supports encoding into a Tensor.
+        """
         encoded = [self.encode(x, tensor=True) for x in targets]
         return torch.stack(encoded, dim=0)
 
