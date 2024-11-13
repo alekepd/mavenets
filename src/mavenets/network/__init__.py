b9518e67fb26452913a4044d0727acc586114a87
Aleksander Durumeric <alekepd@gmail.com>
2024-11-13 05:37:23 -0600

    Made more network classes visible.

--- src/mavenets/network/__init__.py
+++ src/mavenets/network/__init__.py
@@ -1,4 +1,5 @@
 """Provides trainable networks."""
 from .transformer import SumTransformer  # noqa: F401
-from .tune import MHTuner, FFNTuner, SharedFanTuner  # noqa: F401
+from .tune import MHTuner, FFNTuner, SharedFanTuner, NullTuner  # noqa: F401
 from .mpn import GraphNet  # noqa: F401
+from .base import MLP # noqa: F401
