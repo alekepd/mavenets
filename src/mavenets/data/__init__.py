bf2107e781ec24fafb4cf3660f63c9a3b3abe0ae
Aleksander Durumeric <alekepd@gmail.com>
2024-11-15 06:04:50 -0600

    Added exports to data submodule init.

--- src/mavenets/data/__init__.py
+++ src/mavenets/data/__init__.py
@@ -1,9 +1,10 @@
 """Provides interface to training, validation, and test data."""
 from .load import get_datasets  # noqa: F401
 from .spec import (
-    resolve_dataspec, # noqa: F401
-    DataSpec, # noqa: F401
-    MAX_DATASPEC_INDEX, # noqa: F401
-    DATA_SPECS, # noqa: F401
-)  
+    resolve_dataspec,  # noqa: F401
+    DataSpec,  # noqa: F401
+    MAX_DATASPEC_INDEX,  # noqa: F401
+    DATA_SPECS,  # noqa: F401
+)
 from .graph import LegacyGraphDataReader  # noqa: F401
+from .featurize import get_default_int_encoder, SARS_COV2_SEQ  # noqa: F401
