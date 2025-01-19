b47c78aef0dfada71d253397d38e0d83b88d2eec
Aleksander Durumeric <alekepd@gmail.com>
2025-01-19 06:11:54 -0600

    Fixed eval error in sim implementation for max dist

--- src/mavenets/sample/step.py
+++ src/mavenets/sample/step.py
@@ -302,7 +302,7 @@ class MetStep:
         if self.bcast_center is not None:
             # get which samples would be far
             mask = (
-                num_changes(self.bcast_center, variates) < self.max_distance_to_center
+                num_changes(self.bcast_center, cands) < self.max_distance_to_center
             )
             # boolean and
             acceptances = acceptances * mask
