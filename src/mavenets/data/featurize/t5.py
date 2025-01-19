cff86ddb732928f0e6886be62573faa831ce2c76
Aleksander Durumeric <alekepd@gmail.com>
2025-01-19 13:04:40 -0600

    Made featurization use protein-wise averaging

--- src/mavenets/data/featurize/t5.py
+++ src/mavenets/data/featurize/t5.py
@@ -7,6 +7,16 @@ from torch import Tensor
 from transformers import T5Tokenizer, T5EncoderModel  # type:ignore
 import torch
 
+def chunks(inp: Sequence, n: int):
+    """Yield successive n-sized chunks from inp.
+
+    From stack overflow:
+    https://stackoverflow.com/questions/312443/
+    how-do-i-split-a-list-into-equally-sized-chunks
+    """
+    for i in range(0, len(inp), n):
+        yield inp[i:(i + n)]
+
 
 def _example(device: str = "cuda") -> Tensor:
     tokenizer = T5Tokenizer.from_pretrained(
@@ -46,7 +56,12 @@ class T5EncoderWrapper:
     T5_huggingface_name: Final = "Rostlab/prot_t5_xl_half_uniref50-enc"
 
     def __init__(
-        self, integer_encoder: IntEncoder, device: str, flatten: bool = True
+        self,
+        integer_encoder: IntEncoder,
+        device: str,
+        flatten: bool = True,
+        per_protein: bool = True,
+        batch_size: int = 32,
     ) -> None:
         """Store options.
 
@@ -60,9 +75,18 @@ class T5EncoderWrapper:
         device:
             torch device identifier (e.g., "cuda")
         flatten:
-            If true, we return a matrix of shape (b, f) for batched input. Otherwise,
-            the returned values are organized by sequence (with an additional entry),
-            shape (b, n_res+1, 1024)
+            If per_protein is True, ignored. Else, if true, we return a matrix
+            of shape (b, f) for batched input. Otherwise, the returned values
+            are organized by sequence (with an additional entry), shape (b,
+            n_res+1, 1024)
+        per_protein:
+            If True, we average over all residues in each sample to produce a single
+            1024-sized vector. Setting this to False keeps embeddings separate per
+            amino acid. False is more expressive but may have a massive memory
+            footprint.
+        batch_size:
+            Number of examples to process at once. Bigger values use more memory
+            and probably don't help performance.
 
         """
         if integer_encoder is None:
@@ -76,9 +100,11 @@ class T5EncoderWrapper:
         # Load the model
         self.t5 = T5EncoderModel.from_pretrained(self.T5_huggingface_name).to(device)
         self.integer_encoder = integer_encoder
+        self.per_protein = per_protein
+        self.batch_size = batch_size
 
-    def batch_encode(self, int_encoded: Tensor) -> Tensor:
-        """Encode batch of sequences.
+    def vectorized_encode(self, int_encoded: Tensor) -> Tensor:
+        """Encode iterable of sequences.
 
         Arguments:
         ---------
@@ -104,7 +130,32 @@ class T5EncoderWrapper:
 
         emb = embedding_repr.last_hidden_state
 
-        if self.flatten:
+        if self.per_protein:
+            # average over residues, not batch examples
+            return emb.mean(dim=1)
+        elif self.flatten:
             return emb.flatten(start_dim=1)
         else:
             return emb
+
+    def batch_encode(self, int_encoded: Tensor) -> Tensor:
+        """Encode iterable of sequences.
+
+        Unlike vectorized_encode, input is processed in chunks.
+
+        Arguments:
+        ---------
+        int_encoded:
+            Tensor of shape (b, d): b specifies the sample (i.e., batch dimension). Each
+            element is an integer encoded (not one-hot) encoded element. Should
+            be encoded using the integer_encoder created an initialization.
+
+        Returns:
+        -------
+        Returns Tensor with first dimension the batch dimension.
+
+        """
+        processed = []
+        for piece in chunks(int_encoded,self.batch_size):
+            processed.append(self.vectorized_encode(piece))
+        return torch.concatenate(processed,axis=0) # type: ignore
