a2ef55460bfc3e8ff2eef4e9907c2b9060fc7493
Aleksander Durumeric <alekepd@gmail.com>
2025-01-19 08:58:15 -0600

    Added missing import.

--- src/mavenets/data/featurize/__init__.py
+++ src/mavenets/data/featurize/__init__.py
@@ -1,7 +1,8 @@
 """Provides tools for transforming sequences in features."""
 from .core import (
+    get_alphabet,  # noqa: F401
     get_default_int_encoder,  # noqa: F401
-    IntEncoder, # noqa: F401
+    IntEncoder,  # noqa: F401
     int_to_floatonehot,  # noqa: F401
     SARS_COV2_SEQ,  # noqa: F401
 )
