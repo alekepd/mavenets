94fff9fd434a4bb439957f32de5c9f746e1adde6
Aleksander Durumeric <alekepd@gmail.com>
2025-03-02 09:57:21 -0600

    Added lrmlp

--- src/mavenets/network/__init__.py
+++ src/mavenets/network/__init__.py
@@ -3,3 +3,4 @@ from .transformer import SumTransformer  # noqa: F401
 from .tune import MHTuner, FFNTuner, SharedFanTuner, NullTuner  # noqa: F401
 from .mpn import GraphNet  # noqa: F401
 from .base import MLP # noqa: F401
+from .nontraditional import LRMLP # noqa: F401
