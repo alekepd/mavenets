60ae90668a8aef774c03e4eb5b8220e79752d7d9
Aleksander Durumeric <alekepd@gmail.com>
2025-04-06 22:22:28 -0500

    Added linear tuner class to submodule init.

--- src/mavenets/network/__init__.py
+++ src/mavenets/network/__init__.py
@@ -1,6 +1,12 @@
 """Provides trainable networks."""
 from .transformer import SumTransformer  # noqa: F401
-from .tune import MHTuner, FFNTuner, SharedFanTuner, NullTuner  # noqa: F401
+from .tune import (
+    MHTuner,  # noqa: F401
+    FFNTuner,  # noqa: F401
+    SharedFanTuner,  # noqa: F401
+    NullTuner,  # noqa: F401
+    LinearTuner,  # noqa: F401
+)
 from .mpn import GraphNet  # noqa: F401
-from .base import MLP # noqa: F401
-from .nontraditional import LRMLP # noqa: F401
+from .base import MLP  # noqa: F401
+from .nontraditional import LRMLP  # noqa: F401
