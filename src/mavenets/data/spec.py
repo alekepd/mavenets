04022f1fdaa33e7cd2d735c9b66aafe251856c43
Aleksander Durumeric <alekepd@gmail.com>
2026-01-31 19:12:32 -0600

    Fixed bad valid reference in small 12 dataset.

--- src/mavenets/data/spec.py
+++ src/mavenets/data/spec.py
@@ -170,7 +170,7 @@ ALT_DATA_SPECS: Final = [
     DataSpec(
         name="small_base_mutsplit_12",
         train_filename=Path("base_mut12_train_small.csv"),
-        valid_filename=Path("base_mut12_train_small.csv"),
+        valid_filename=Path("base_mut12_valid_small.csv"),
         test_filename=Path("test_data.csv"),
         index=18,
     ),
